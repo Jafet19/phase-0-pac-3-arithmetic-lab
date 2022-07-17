e8580fc60be9cf32370eb8ac9d9d9808d6306baf
Jafet19 <kacur99@gmail.com>
2022-07-17 13:45:02 -0500

    commit tree f5df3725183fb3ca0ce040eebb024b89b8660d21

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    n++
+    return (n);
+}
+function decrement(n){
+    --n
+    return(n);
+}
+function makeInt(str){
+    return parseInt(str, 10);
+}
+function preserveDecimal(str){
+    return parseFloat(str,10);
+}
